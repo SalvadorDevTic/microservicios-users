2aa54acec3fbcdd3b9715c594e1cc145612577a8
salvador <salvadormartinez.dev@gmail.com>
2025-10-27 18:27:26 +0100

    Inicializar proyecto base con Spring Initializr

---
+++ src/main/java/com/salvador/microservicios/users/microservicios_users/MicroserviciosUsersApplication.java
@@ -0,0 +1,13 @@
+package com.salvador.microservicios.users.microservicios_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salvador/microservicios/users/microservicios_users/MicroserviciosUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvador.microservicios.users.microservicios_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
